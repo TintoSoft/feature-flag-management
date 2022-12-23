82ef9a871bd9db8546737f2348493392572e419a
TintoSoft <tintosoftdev@gmail.com>
2022-12-23 22:54:06 +0100

    remove console logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-const createConfig = require("./createConfig");
 const featureFlagsService = require("./featureFlagsService");
-createConfig.loadFeatureFlags({ prueba: true });
-const resp = featureFlagsService.getFlagStatus("prueba");
-console.log({ resp });
+
 module.exports = { featureFlagsService };
